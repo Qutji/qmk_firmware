7a400ae3a44da98599f24c128f6360384dae7df6
yasuhiro <yasuh1r0.7a@gmail.com>
2016-07-31 18:44:18 +0900

    fix underbar keymap

--- keyboards/ergodox_ez/keymaps/raindropops/keymap.c
+++ keyboards/ergodox_ez/keymaps/raindropops/keymap.c
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                      KC_TRNS,
                                                      KC_SPC, KC_ENT, KC_TRNS,
        // right hand
-       KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_RO,
+       KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_UNDS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS,  KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
