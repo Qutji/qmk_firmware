6836e6ba26e0f519ff9e3fd48c2ae6bee2964056
yasuhiro <yasuhiro@mbp2013late.local>
2017-01-03 21:20:37 +0900

    change mouse speed

--- keyboards/ergodox/config.h
+++ keyboards/ergodox/config.h
@@ -3,8 +3,8 @@
 
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    5
-#define MOUSEKEY_MAX_SPEED      50
+#define MOUSEKEY_TIME_TO_MAX    10
+#define MOUSEKEY_MAX_SPEED      25
 #define MOUSEKEY_WHEEL_DELAY 0
 
 #define TAPPING_TOGGLE  1
